01b0d7494c6bdbb3122b2facaa7c6999d07b7018
Henok Teixeira <henokteixeira@gmail.com>
2025-04-22 00:32:31 -0300

    feat: add notifications app with send mail service

---
+++ notifications/services/base.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from ..types.email import EmailParams
+
+
+class EmailService(ABC):
+    @abstractmethod
+    def send_email(self, email_params: EmailParams) -> bool:
+        pass
---
+++ notifications/services/mailersend.py
@@ -0,0 +1,64 @@
+from typing import List, Dict, Any
+from django.conf import settings
+from mailersend import emails
+
+from .base import EmailService
+from ..exceptions import EmailServiceError
+from ..types.email import EmailParams
+
+
+class MailerSendService(EmailService):
+    def __init__(self):
+        self.api_key = settings.MAILERSEND_API_KEY
+        self.default_from_email = settings.DEFAULT_FROM_EMAIL
+        self.default_from_name = settings.DEFAULT_FROM_NAME
+        self._mailer = None
+
+    @property
+    def mailer(self):
+        if self._mailer is None:
+            self._mailer = emails.NewEmail(self.api_key)
+        return self._mailer
+
+    def _prepare_recipients(self, emails_list: List[str]) -> List[Dict[str, str]]:
+        return [{"email": email} for email in emails_list]
+
+    def _prepare_email(self, email_params: EmailParams) -> Dict[str, Any]:
+        body = {}
+
+        mail_from = {
+            "email": self.default_from_email,
+            "name": self.default_from_name,
+        }
+
+        reply_to = {
+            "email": email_params.from_email or self.default_from_email,
+            "name": email_params.from_name or self.default_from_name,
+        }
+
+        recipients = self._prepare_recipients(email_params.to_email)
+        cc_list = (
+            self._prepare_recipients(email_params.cc) if email_params.cc else None
+        )
+
+        self.mailer.set_mail_from(mail_from, body)
+        self.mailer.set_mail_to(recipients, body)
+        self.mailer.set_reply_to(reply_to, body)
+
+        if cc_list:
+            self.mailer.set_cc_recipients(cc_list, body)
+
+        self.mailer.set_subject(email_params.subject, body)
+        self.mailer.set_plaintext_content(email_params.content, body)
+
+        return body
+        
+    def send_email(self, email_params: EmailParams) -> bool:
+        try:
+            body = self._prepare_email(email_params)
+            response = self.mailer.send(body)
+            
+            return response.strip() == "202"
+
+        except RuntimeError as e:
+            raise EmailServiceError(f"Falha ao enviar email: {str(e)}")
