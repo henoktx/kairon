34941a6db737e92bf596623977a39a45b76d0d22
Henok Teixeira <henokteixeira@gmail.com>
2025-04-22 00:34:49 -0300

    feat: add project temporal activities

---
+++ temporal/activities.py
@@ -0,0 +1,58 @@
+from typing import Dict, Any
+from asgiref.sync import sync_to_async
+
+from temporalio import activity
+
+from executions.services.update_task_execution import (
+    update_task_execution_status,
+)
+from executions.services.update_execution import update_execution_status
+from executions.types.task_execution import UpdateTaskExecutionParams
+from executions.types.execution import UpdateExecutionParams
+
+from notifications.services.mailersend import MailerSendService
+from notifications.exceptions import EmailServiceError
+from notifications.types.email import EmailParams
+
+
+@activity.defn
+async def update_task_status_activity(
+    task_execution_params: UpdateTaskExecutionParams,
+) -> Dict[str, Any]:
+    try:
+        result = await sync_to_async(update_task_execution_status)(
+            task_execution_params
+        )
+        return result
+    except Exception as e:
+        raise RuntimeError(f"Erro ao atualizar o status da task: {str(e)}")
+
+
+@activity.defn
+async def update_execution_status_activity(
+    execution_params: UpdateExecutionParams,
+) -> None:
+    try:
+        await sync_to_async(update_execution_status)(execution_params)
+    except Exception as e:
+        raise RuntimeError(f"Erro ao atualizar o status do workflow: {str(e)}")
+
+
+@activity.defn
+async def send_email_activity(email_params: EmailParams) -> None:
+    email_service = MailerSendService()
+
+    try:
+        success = await sync_to_async(email_service.send_email)(email_params)
+
+        if not success:
+            raise RuntimeError("Falha ao enviar e-mail")
+
+        # return {
+        #     "status": "sent",
+        #     "to_email": email_params.to_email,
+        #     "subject": email_params.subject,
+        #     "timestamp": datetime.now().isoformat(),
+        # }
+    except EmailServiceError as e:
+        raise RuntimeError(f"Erro no serviço de e-mail: {str(e)}")
