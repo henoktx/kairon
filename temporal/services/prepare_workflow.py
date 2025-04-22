adb29f9965e022762c565a47c4eed2bd0d7d2d4a
Henok Teixeira <henokteixeira@gmail.com>
2025-04-22 00:35:52 -0300

    feat: add prepare workflow service

---
+++ temporal/services/prepare_workflow.py
@@ -0,0 +1,47 @@
+from temporalio.common import RetryPolicy
+
+from executions.models import Execution, TaskExecution
+from workflows.models import EmailTask
+from ..types.workflow import WorkflowInput
+from ..types.task import TaskData, TaskType
+from notifications.types.email import EmailParams
+
+
+def prepare_workflow_input(execution: Execution) -> WorkflowInput:
+    tasks_data = []
+    tasks_execution = TaskExecution.objects.filter(execution_id=execution.id)
+
+    for task_execution in tasks_execution:
+        task = task_execution.task
+        task_data = TaskData(
+            task_execution_id=task_execution.id,
+            name=task.name,
+            order=task.order,
+            task_type=task.task_type,
+            retry_policy=RetryPolicy(
+                initial_interval=task.inital_interval,
+                maximum_attempts=task.maximum_attempts,
+                backoff_coefficient=task.backoff
+            )
+        )
+
+        if task.task_type == TaskType.EMAIL:
+            email_task = EmailTask.objects.get(task_id=task.id)
+            email_config = EmailParams(
+                to_email=email_task.get_recipients_list(),
+                subject=email_task.subject,
+                content=email_task.content,
+                cc=email_task.get_cc_list(),
+                from_email=email_task.task.created_by.email,
+                from_name=email_task.task.created_by.first_name
+            )
+            task_data.email_config = email_config
+
+        tasks_data.append(task_data)
+
+    return WorkflowInput(
+        execution_id=execution.id,
+        workflow_name=execution.workflow.name,
+        tasks=sorted(tasks_data, key=lambda x: x.order),
+        delay_seconds=execution.workflow.delay_seconds,
+    )
