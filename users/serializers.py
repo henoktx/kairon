5a685eac81bf8344ef2b422ab723df1b93c2f37c
Henok Teixeira <henokteixeira@gmail.com>
2025-04-22 00:39:08 -0300

    fix: fix create new users enpoint

--- users/serializers.py
+++ users/serializers.py
@@ -8,3 +8,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id", "first_name", "username", "password", "email"]
         extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -1,20 +1,27 @@
+from typing import override
 from rest_framework import viewsets, mixins, permissions, generics
 
 from .models import User
 from .serializers import UserSerializer
 
 
-class CreateUserView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = UserSerializer
+# class CreateUserView(generics.CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+#     def get_permissions(self):
+#         self.permission_classes = [permissions.AllowAny]
+#         return super().get_permissions()
 
 
-class UpdateDestroyRetrieveUserViewSet(
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet,
-):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
